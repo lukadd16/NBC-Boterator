fbec61fc90e8de597149e78220e3377624471fba
lukadd <fancyman444@gmail.com>
2020-12-22 18:46:01 -0500

    docs: Add example configuration file

---
+++ config.example.py
@@ -0,0 +1,59 @@
+# Discord Application Token
+BOT_TOKEN = "your-token-here"
+
+# Bot Prefix
+BOT_PREFIX = "jj "
+
+# Initial Extensions
+BOT_EXTENSIONS = [
+    "cogs.help",
+    "cogs.newhelp",
+    "cogs.owner",
+    "cogs.utilities",
+    "cogs.status_loop",
+    "cogs.error_handler",
+    "cogs.moderation",
+    "cogs.fun"
+]
+
+# Emojis used by the bot
+BOT_EMOJI_ARROW = "<:name_of_emoji:ID>"
+BOT_EMOJI_BTAG =
+BOT_EMOJI_ONLINE =
+BOT_EMOJI_IDLE =
+BOT_EMOJI_DND =
+BOT_EMOJI_OFFLINE =
+BOT_EMOJI_STREAM =
+BOT_EMOJI_UPVOTE =
+BOT_EMOJI_DOWNVOTE =
+
+# Embed Constants
+BOT_VERSION = "V1.0.0"
+BOT_FEATURES = "Initial Release"
+BOT_URL = "http://northbridgecafe.tk"
+BOT_AUTHOR_NAME = "NBC Boterator"
+BOT_AUTHOR_CLICK = "Click to visit our website!"
+BOT_OLDFOOTER = "This bot was created by @Lukadd.16#8870, please DM him if you have any bugs or issues to report."
+BOT_FOOTER = f"Found a bug/have an idea? Send it to the dev with {BOT_PREFIX}suggest"
+BOT_HELP_USER_ARG = "```Argument is optional but if specified must be a: @mention, userID, or username#discriminator```"
+BOT_HELP_REASON_ARG = "```Optional reason argument is a message that will be appended in the server audit log for other moderators to see, if none is provided, a default one will be used.```"
+BOT_HELP_BAN_ARG = "```Optional days argument is how many days prior (to a max of 7) that the bot will delete messages sent by the specified user.```"
+
+# Embed Colours
+BOT_COLOUR = 0x1cc2ff
+BOT_ERR_COLOUR = 0xd80000
+BOT_SUCCESS_COLOUR = 0x00e600
+
+LOG_LEVEL = "info"
+
+# Home Server
+HOME_SERVER_ID =
+
+# Status Channel (successful login, successful shutdown, etc.)
+STATUS_CHANNEL_ID =
+
+# Event Logging Channel (joined new server, critical errors, etc.)
+EVENTS_CHANNEL_ID =
+
+# Suggestion Channel
+SUGGEST_CHANNEL_ID =
