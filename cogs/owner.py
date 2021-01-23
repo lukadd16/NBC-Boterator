f90e4f7c991b1da34b833032fcd68e6bece3b0f6
lukadd <lukadd@icloud.com>
2021-01-22 21:01:16 -0500

    feature(owner): Restructure reload cmds using subcmds; rename methods; change owner check to be global to the cog

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,85 +1,95 @@
-# Description: Cog that houses owner only commands (inspired by EvieePy)
+# Description: Cog that houses owner-only commands (inspired by EvieePy)
 
 from datetime import datetime
 from discord.ext import commands
 from importlib import reload as importlib_reload
+from typing import Optional
 from utils import botUtils
+
 
 class OwnerCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_result = None
 
+    # Overwrite cog_check method
+    # Commands within this cog can only be used by the bot owner
+    async def cog_check(self, ctx):
+        if not await ctx.bot.is_owner(ctx.author):
+            raise commands.NotOwner()
+        return True
+
     # Load an individual cog; use dot path notation -> Ex: jj load cogs.help
     @commands.command(aliases=["load", "loadcog"])
-    @commands.is_owner()
-    async def bog_load(self, ctx, *, cog: str):
+    async def cload(self, ctx, *, cog: str):
         try:
             self.bot.load_extension(cog)
         except Exception as e:
-            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send("**`SUCCESS`**")
 
-    # Unload an individual cog; use dot path notation
+    # Unload an individual cog
     @commands.command(aliases=["unload", "unloadcog"])
-    @commands.is_owner()
-    async def bog_unload(self, ctx, *, cog: str):
+    async def cunload(self, ctx, *, cog: str):
         try:
             self.bot.unload_extension(cog)
         except Exception as e:
-            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send("**`SUCCESS`**")
 
-    # Reload an individual cog; use dot path notation
-    @commands.command(aliases=["reload", "reloadcog"])
-    @commands.is_owner()
-    async def bog_reload(self, ctx, *, cog: str):
+    # Reload an individual cog, config file or botUtils.py module into memory
+    @commands.group(aliases=["reload"])
+    async def creload(self, ctx, *, cog: Optional[str] = None):
+        # Handle user input
+        if cog is None or ctx.invoked_subcommand is None:
+            await ctx.send(
+                "You did not specify a cog or subcommand. Valid subcommands "
+                "are:\n>>> `all`\n`config`\n`utils`")
+
         try:
             self.bot.reload_extension(cog)
         except Exception as e:
-            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send("**`SUCCESS`**")
 
-    # Reloads the bot's config file into memory without needing to restart the
-    # bot (finally!)
+    # Reload all cogs + botUtils.py module + config file
+    # Useful when pushing non-breaking changes to the production server that
+    # span across multiple files
     @commands.command()
-    @commands.is_owner()
-    async def reloadconf(self, ctx):
-        try:
-            importlib_reload(self.bot.config)
-        except Exception as e:
-            await ctx.send(f'**`ERROR`** {type(e).__name__} - {e}')
-        else:
-            await ctx.send('**`SUCCESS`**')
-
-    # Reloads the bot's botUtils module into memory
-    @commands.command()
-    @commands.is_owner()
-    async def reloadutils(self, ctx):
-        try:
-            importlib_reload(botUtils)
-        except Exception as e:
-            await ctx.send(f'**`ERROR`** {type(e).__name__} - {e}')
-        else:
-            await ctx.send('**`SUCCESS`**')
-
-    # Reload all cogs, utils & config file, useful when pushing non-breaking
-    # changes to the production server that span across multiple cogs
-    @commands.command(aliases=["reload_all", "rall"])
-    @commands.is_owner()
-    async def bog_reload_all(self, ctx):
+    async def all(self, ctx):
         try:
             for extension in self.bot.config.BOT_EXTENSIONS:
                 self.bot.reload_extension(extension)
             importlib_reload(self.bot.config)
             importlib_reload(botUtils)
         except Exception as e:
-            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send("**`SUCCESS`**")
+
+    # Reloads the bot's config file into memory without needing to restart the
+    # bot (finally!)
+    @creload.command()
+    async def config(self, ctx):
+        try:
+            importlib_reload(self.bot.config)
+        except Exception as e:
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
+        else:
+            await ctx.send("**`SUCCESS`**")
+
+    # Reloads the botUtils.py module into memory
+    @commands.command()
+    async def utils(self, ctx):
+        try:
+            importlib_reload(botUtils)
+        except Exception as e:
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
+        else:
+            await ctx.send("**`SUCCESS`**")
 
     # TODO: Add args to this cmd, takes status & reason, displays these
     #       in an elegant embed that contains status emojis, etc.
@@ -88,7 +98,6 @@ class OwnerCog(commands.Cog):
     #       but for use in announcing planned downtime, known issues, etc.
     # Gracefully shutdown the bot; calculate & report uptime
     @commands.command(aliases=["kill", "terminate"])
-    @commands.is_owner()
     async def shutdown(self, ctx):
         await ctx.send("Shutting down...")
         await self.bot.status_channel.send(
@@ -101,13 +110,14 @@ class OwnerCog(commands.Cog):
 
         # Call conversion method & report uptime
         await self.bot.status_channel.send(
-            'Total Uptime was: '
-            f'`{botUtils.convert_seconds_friendly(delta_uptime_seconds)}`'
+            "Total Uptime was: "
+            f"`{botUtils.convert_seconds_friendly(delta_uptime_seconds)}`"
         )
         print("\n[BT] Disconnected Gracefully")
         await self.bot.logout()
 
     # If and when DB is added, manual SQL execution command can be put here
+
 
 def setup(bot):
     bot.add_cog(OwnerCog(bot))
